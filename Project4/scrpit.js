20c5bd1e39cfe5990061cda1335d8d216197b78f
Yogendra Dawar <yogendradawar6@gmail.com>
2024-05-05 17:41:43 +0530

    Scrolling Text Animation

---
+++ Project4/scrpit.js
@@ -0,0 +1,30 @@
+function marqueAnimation(){
+    window.addEventListener("wheel",(dets)=>{
+        if(dets.deltaY > 0){
+            gsap.to(".marque",{
+                transform: 'translateX(-200%)',
+                duration : 4,
+                repeat : -1,
+                ease : "none",
+            })
+    
+            gsap.to(".marque img",{
+                rotate : 180
+            })
+        }
+        else{
+            gsap.to(".marque",{
+                transform: 'translateX(0%)',
+                duration : 4,
+                repeat : -1,
+                ease : "none",
+            })
+    
+            gsap.to(".marque img",{
+                rotate : 0
+            })
+        }
+    })
+}
+
+marqueAnimation();
